8bed7a9c95360f9ba1d196b039bb495e5014101b
Zoe <udokpanzoe@yahoo.com>
2023-06-11 11:58:25 +0100

    Update profit_calculator.py

--- profit_calculator.py
+++ profit_calculator.py
@@ -1,70 +1,33 @@
-def calculate_arbitrage_allocation(capital, odds, is_three_way=False):
+def calculate_arbitrage_allocation(capital, odds):
+    num_outcomes = len(odds)
+    assert num_outcomes >= 2, "At least two odds are required for betting."
 
-    if is_three_way:
+    implied_probabilities = [1 / odd for odd in odds]
+    total_implied_probability = sum(implied_probabilities)
+    allocations = [(prob / total_implied_probability) * capital for prob in implied_probabilities]
+    return allocations
 
-        assert len(odds) == 3, "Three odds are required for three-way betting."
+def calculate_potential_profit(capital, odds):
+    allocations = calculate_arbitrage_allocation(capital, odds)
+    potential_profits = [allocations[i] * (odds[i] - 1) for i in range(len(odds))]
+    return potential_profits
 
-        implied_probabilities = [1 / odd for odd in odds]
+def calculate_profit_percentage(capital, odds):
+    allocations = calculate_arbitrage_allocation(capital, odds)
+    total_allocation = sum(allocations)
+    profit_percentage = sum(calculate_potential_profit(capital, odds)) / total_allocation * 100
+    return profit_percentage
 
-        total_implied_probability = sum(implied_probabilities)
-
-        allocations = [(prob / total_implied_probability) * capital for prob in implied_probabilities]
-
-        return allocations
-
-    else:
-
-        assert len(odds) == 2, "Two odds are required for two-way betting."
-
-        implied_probabilities = [1 / odd for odd in odds]
-
-        total_implied_probability = sum(implied_probabilities)
-
-        allocations = [(prob / total_implied_probability) * capital for prob in implied_probabilities]
-
-        return allocations
-
-def calculate_potential_profit(capital, odds, is_three_way=False):
-
-    allocations = calculate_arbitrage_allocation(capital, odds, is_three_way)
-
-    if is_three_way:
-
-        potential_profits = [allocations[i] * (odds[i] - 1) for i in range(3)]
-
-        return potential_profits
-
-    else:
-
-        potential_profit = sum(allocations) * (max(odds) - 1)
-
-        return potential_profit
-
-# Example usage for two-way odds
-
+# Example usage for any number of odds
 capital = 10000
-
-odds = [2.5, 2.8]
-
+odds = [2.5, 2.8, 3.2]
 allocations = calculate_arbitrage_allocation(capital, odds)
+potential_profits = calculate_potential_profit(capital, odds)
+profit_percentage = calculate_profit_percentage(capital, odds)
+total_profit = sum(potential_profits)
 
-potential_profit = calculate_potential_profit(capital, odds)
-
+print(f"Initial Capital: ${capital}")
 print(f"Allocations: {allocations}")
-
-print(f"Potential Profit: {potential_profit}")
-
-# Example usage for three-way odds
-
-capital = 10000
-
-odds = [2.5, 3.0, 4.0]
-
-allocations = calculate_arbitrage_allocation(capital, odds, is_three_way=True)
-
-potential_profits = calculate_potential_profit(capital, odds, is_three_way=True)
-
-print(f"Allocations: {allocations}")
-
 print(f"Potential Profits: {potential_profits}")
-
+print(f"Profit Percentage: {profit_percentage:.2f}%")
+print(f"Total Profit: {total_profit}%")
